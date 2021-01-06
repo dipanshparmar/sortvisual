58612d613fb73c8a1011ed140364da27b73e5482
Dipansh Parmar <dipansh@pop-os.localdomain>
2021-01-06 21:40:48 +0530

    Added sidebar for extra algos

---
+++ js/sideBars.js
@@ -0,0 +1,31 @@
+// getting the extra algos container toggler
+const toggler = document.querySelector('#extra-algos-container-toggler');
+const algosContainer = document.querySelector('#extra-algos-container');
+const algoButtons = document.querySelectorAll('#extra-algos-container span');
+
+// listening for hover on toggler
+toggler.addEventListener('mouseover', () => {
+    algosContainer.style.transform = "translate(0)"; // showing the container
+})
+
+// listening for mouseout on toggler
+toggler.addEventListener('mouseout', () => {
+    algosContainer.style.transform = "translate(-100%)"; // hiding the container
+})
+
+// listening for hover on algos container
+algosContainer.addEventListener('mouseover', () => {
+    algosContainer.style.transform = "translate(0)"; // showing the container
+})
+
+// listening for mouseout on hover container
+algosContainer.addEventListener('mouseout', () => {
+    algosContainer.style.transform = "translate(-100%)"; // showing the container
+})
+
+// when there is a click on the algorithms of the side container, then close the sidebar
+algoButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        algosContainer.style.transform = "translate(-100%)"; // hiding the container
+    })
+})
