f8f4ef9405720615bd0ff9b5e63783f40729b6d0
Dipansh Parmar <dipansh@pop-os.localdomain>
2021-01-07 20:08:00 +0530

    Added reload functionality

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// adding some extra stuff here
+const logoText = document.querySelector('.logo-text');
+
+// when there is a click on logo, then reload the page
+logoText.addEventListener('click', () => {
+    window.location.reload();
+})
