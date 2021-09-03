2cc4a03d684f8a06024446146960daf1e0d1f16d
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-09-03 10:37:36 +0530

    added responsiveness

--- js/main.js
+++ js/main.js
@@ -36,4 +36,38 @@ function removeComplexities() {
     sb.textContent = '?';
 }
 
+// displaying the options on click on the hamburger and hiding them on another click
+let isOptionsOpen = false; // this will help to make decisions
+document.querySelector('#hamburger img').addEventListener('click', () => {
+    // getting the options
+    const options = document.querySelector('#options');
+
+    if (isOptionsOpen) {
+        // hide the options if already open
+        options.style.display = 'none';
+    } else {
+        // otherwise show the options and set isOptionsOpen to true
+        options.style.display = 'flex';
+    }
+
+    // changing the value of isOptionsOpen
+    isOptionsOpen = !isOptionsOpen;
+})
+
+// display the controls on click on the hamburger and hiding them on another click
+let isControlsOpen = false;
+document.querySelector('#controls-hamburger img').addEventListener('click', () => {
+    // getting the controls
+    const controls = document.querySelector('#controls');
+
+    if(isControlsOpen) {
+        controls.style.display = 'none';
+    } else {
+        controls.style.display = 'flex';
+    }
+
+    // updating the value
+    isControlsOpen = !isControlsOpen;
+})
+
 export { addComplexities, removeComplexities };
